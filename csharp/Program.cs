d8a847087c003c0d61cc0d128f12d8b4dd86e7ba
isaacaflores2 <isaac.a.flores2@gmail.com>
2021-01-11 11:08:09 -0800

    add dotnet/csharp hello

---
+++ csharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO; 
+
+namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args == null || args.Length < 1){
+                Console.WriteLine("Please provide a file path with the hello text...");
+            }
+
+            string path = args[0];
+            
+            if(!File.Exists(path)){
+                throw new FileNotFoundException($"Could not find file at {path}");
+            }
+
+            string helloText = File.ReadAllText(path);
+            string updatedHello = updateHelloText(helloText);
+
+            Console.WriteLine(updatedHello);
+        }
+
+        private static string updateHelloText(string text){
+            string updatedText = text.Replace("shell command", "csharp program");
+            return updatedText;
+        }
+    }
+}
